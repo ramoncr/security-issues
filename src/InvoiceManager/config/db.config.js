7980a32863914668386f3cdf43281d08dde0f0b4
Ramon <ramoncr@users.noreply.github.com>
2024-01-15 15:45:08 +0100

    fix: fixed resource exchaustion

--- src/InvoiceManager/config/db.config.js
+++ src/InvoiceManager/config/db.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "123456",
+  PASSWORD: "I7cpOBw45waP8B4Htdf5D6g",
   DB: "testdb",
   dialect: "mysql",
   pool: {
-    max: 5,
+    max: 6,
     min: 0,
     acquire: 30000,
     idle: 10000
