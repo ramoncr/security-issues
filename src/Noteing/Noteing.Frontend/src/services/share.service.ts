fdfb817be2ba914133f6a46511f99a27b9031ed4
ramonyd <118173456+ramonyd@users.noreply.github.com>
2024-07-31 15:13:12 +0200

    cleanup of service

--- src/Noteing/Noteing.Frontend/src/services/share.service.ts
+++ src/Noteing/Noteing.Frontend/src/services/share.service.ts
@@ -3,7 +3,6 @@ import type { Note } from "@/models";
 
 function createShareLink(note: Note) {
     const host = window.location.host;
-
     const payload = {
         url: '/notes/'+ note.id + '/share',
         content: { noteId: note.id, sharer: note.owner },
@@ -11,16 +10,12 @@ function createShareLink(note: Note) {
 
     const payloadString = JSON.stringify(payload);
     const payloadEncoded = btoa(payloadString);
-
     return `${host}/share/${payloadEncoded}`;
 }
-
 
 async function resolveSharePayload(encodedPayload: string) {
     const payloadString = atob(encodedPayload);
     const payload = JSON.parse(payloadString);
-
-    console.log(payload);
     await api.post(payload.url, payload.content);
 }
 
