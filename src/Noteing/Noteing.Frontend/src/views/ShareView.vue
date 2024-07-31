d0b3c5014c1aae55a789f72ebf584ac35f22634e
ramonyd <118173456+ramonyd@users.noreply.github.com>
2024-07-31 15:17:46 +0200

    added improved feedback on shareview

--- src/Noteing/Noteing.Frontend/src/views/ShareView.vue
+++ src/Noteing/Noteing.Frontend/src/views/ShareView.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="m-2">
-        <h3>Share</h3>
-       </div>
-
+  <div class="m-2">
+    <h3 v-if="loading">Accepting share request, we are providing you access.</h3>
+    <div v-if="!loading">
+      <h3>Share request accepted</h3>
+      <p>You can now view the note.</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { resolveSharePayload } from '@/services/share.service';
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { resolveSharePayload } from '@/services/share.service'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
-
-const route = useRoute();
-
+const route = useRoute()
+const loading = ref(true)
 
 onMounted(async () => {
-    const payload = route.params.payload;
+  await handleShareLink()
+})
 
-    await resolveSharePayload(payload.toString());
-});
-
-
-
-
-
-
+async function handleShareLink() {
+  const payload = route.params.payload
+  await resolveSharePayload(payload.toString())
+  loading.value = false
+}
 </script>
